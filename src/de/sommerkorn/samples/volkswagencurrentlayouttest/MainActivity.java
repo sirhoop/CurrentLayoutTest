31608bcbe23f04e3a40916221f07f30e7462bae7
Alexander Sommerkorn <a.sommerkorn@googlemail.com>
2014-06-29 14:55:20 +0200

    first running layout
    gray list layout with three sample items

--- src/de/sommerkorn/samples/volkswagencurrentlayouttest/MainActivity.java
+++ src/de/sommerkorn/samples/volkswagencurrentlayouttest/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TableLayout;
 
 public class MainActivity extends Activity {
 
-//	private TableLayout tableOfInfoTiles;
+	// private TableLayout tableOfInfoTiles;
 	private ListView listOfInfoTiles;
 	private InfoTileAdapter infoTileAdapter;
 	private ArrayList<InfoTileModel> listOfTileModels;
@@ -21,24 +21,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-				
-		this.listOfInfoTiles = (ListView) this.findViewById(R.id.listOfInfoTiles);
-		
+
+		this.listOfInfoTiles = (ListView) this
+				.findViewById(R.id.listOfInfoTiles);
+
 		this.listOfTileModels = new ArrayList<InfoTileModel>();
 		this.listOfTileModels.add(new InfoTileModel("InformationTile 1"));
 		this.listOfTileModels.add(new InfoTileModel("Aktuelle Kalenderinfos"));
 		this.listOfTileModels.add(new InfoTileModel("Speisepläne"));
-		
-		this.infoTileAdapter = new InfoTileAdapter(getApplicationContext(), this.listOfTileModels);
-		
+
+		this.infoTileAdapter = new InfoTileAdapter(getApplicationContext(),
+				this.listOfTileModels);
+
 		this.listOfInfoTiles.setAdapter(infoTileAdapter);
-		
-//		this.tableOfInfoTiles = (TableLayout) findViewById(R.id.listOfInfoTiles);
-//
-//		View infoTile = this.getLayoutInflater().inflate(R.id.infoTile,
-//				this.tableOfInfoTiles);
-//
-//		this.tableOfInfoTiles.addView(infoTile);
 
 	}
 
