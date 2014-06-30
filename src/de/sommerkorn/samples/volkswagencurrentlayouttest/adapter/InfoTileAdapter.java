5dfbf3fa1551f86ec59d2928c9ba4c79614b71bb
Alexander Sommerkorn <a.sommerkorn@googlemail.com>
2014-06-30 22:00:04 +0200

    Images are shown, just have to figure out the correct layout_weight.

--- src/de/sommerkorn/samples/volkswagencurrentlayouttest/adapter/InfoTileAdapter.java
+++ src/de/sommerkorn/samples/volkswagencurrentlayouttest/adapter/InfoTileAdapter.java
@@ -23,18 +23,13 @@ public class InfoTileAdapter extends BaseAdapter {
 
 	private Context context;
 	private ArrayList<InfoTileModel> listOfInfoTiles;
-	private ArrayList<Boolean> visibilityList;
 
 	TextView contentText;
 
 	public InfoTileAdapter(Context context, ArrayList<InfoTileModel> list) {
+		super();
 		this.context = context;
 		this.listOfInfoTiles = list;
-		this.visibilityList = new ArrayList<Boolean>(
-				this.listOfInfoTiles.size());
-		for (Boolean visible : visibilityList) {
-			visible = true;
-		}
 	}
 
 	@Override
@@ -58,22 +53,23 @@ public class InfoTileAdapter extends BaseAdapter {
 	public View getView(int index, View convertView, ViewGroup parentView) {
 		// if (!this.listOfInfoTiles.get(index).isActive())
 		// return null;
-
+		View tile = convertView;
+		
 		final ViewHolder holder;	
 
-		if (convertView == null) {
+		if (tile == null) {
 
 			LayoutInflater mInflater = (LayoutInflater) context
 					.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-			convertView = (LinearLayout) mInflater.inflate(R.layout.info_tile, null);
+			tile = (LinearLayout) mInflater.inflate(R.layout.info_tile, null);
 			
 			holder = new ViewHolder();
-			holder.imageView = new ImageView(convertView.getContext());
-			holder.contentText = new TextView(convertView.getContext());
+			holder.imageView = new ImageView(tile.getContext());
+			holder.contentText = new TextView(tile.getContext());
 			
-			holder.imageView.setImageResource(R.drawable.chuck);
-			holder.imageView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
-					LayoutParams.MATCH_PARENT));
+			holder.imageView.setImageResource(R.drawable.arnold);
+			holder.imageView.setLayoutParams(new LayoutParams(
+					LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 			holder.imageView.setVisibility(View.VISIBLE);
 
 			holder.contentText.setText("Test, Test, Test");
@@ -82,15 +78,15 @@ public class InfoTileAdapter extends BaseAdapter {
 			holder.contentText.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
 					LayoutParams.WRAP_CONTENT));
 
-			((LinearLayout)convertView).addView(holder.imageView);
-			//((LinearLayout)convertView).addView(holder.contentText);
+			((LinearLayout)tile).addView(holder.imageView);
+			((LinearLayout)tile).addView(holder.contentText);
 			
-			convertView.setTag(holder);
+			tile.setTag(holder);
 		} else {
-			holder = (ViewHolder) convertView.getTag();
+			holder = (ViewHolder) tile.getTag();
 		}
 
-		return convertView;
+		return tile;
 	}
 	
 	static class ViewHolder {
