47f520b65fcf8caa344d65dc4dd67f480245d75a
Alexander Sommerkorn <a.sommerkorn@googlemail.com>
2014-07-06 13:41:33 +0200

    added some functions for calendar access. does not works quite yet.

--- src/de/sommerkorn/samples/volkswagencurrentlayouttest/MainActivity.java
+++ src/de/sommerkorn/samples/volkswagencurrentlayouttest/MainActivity.java
@@ -7,7 +7,10 @@ import de.sommerkorn.samples.volkswagencurrentlayouttest.model.ClockTileModel;
 import de.sommerkorn.samples.volkswagencurrentlayouttest.model.ImageTileModel;
 import de.sommerkorn.samples.volkswagencurrentlayouttest.model.InfoTileModel;
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TableLayout;
@@ -18,6 +21,9 @@ public class MainActivity extends Activity {
 	private ListView listOfInfoTiles;
 	private InfoTileAdapter infoTileAdapter;
 	private ArrayList<InfoTileModel> listOfTileModels;
+
+	private static final String[] COLS = new String[] {
+			CalendarContract.Events.TITLE, CalendarContract.Events.DTSTART };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +37,15 @@ public class MainActivity extends Activity {
 		this.listOfTileModels.add(new ImageTileModel("First Image Tile"));
 		this.listOfTileModels.add(new ClockTileModel("Current Time Widget"));
 		this.listOfTileModels.add(new ImageTileModel("Second Image Tile"));
+
+//		Cursor mCursor = this.getContentResolver().query(CalendarContract.CONTENT_URI, COLS, null, null, null);
+//		Log.d("Cursor: ", mCursor.getString(0));
+//		Log.d("Cursor: ", mCursor.getString(1));
+//		Log.d("Cursor: ", mCursor.getString(2));
+//		Log.d("Cursor: ", mCursor.getString(3));
 		
-//		this.listOfTileModels.get(0).setActive(false);
+
+		// this.listOfTileModels.get(0).setActive(false);
 
 		this.infoTileAdapter = new InfoTileAdapter(getApplicationContext(),
 				this.listOfTileModels);
